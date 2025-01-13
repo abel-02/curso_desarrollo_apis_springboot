a61154325d8469dd374a591cb1cf485046d38378
abel-02 <abelaquino02j@gmail.com>
2025-01-13 11:43:50 -0300

    Creacion del controlador de cliente

---
+++ tp_Final/src/main/java/com/redo/tp_Final/controllers/ProductoController.java
@@ -0,0 +1,25 @@
+package com.redo.tp_Final.controllers;
+
+import com.redo.tp_Final.models.Producto;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductoController {
+    public void crearProducto(){
+
+    }
+    public List<Producto> devolverListaCompleta(){
+        return null;
+    }
+    public Producto devolverUnProducto(){
+       return null;
+    }
+    public void eliminarUnProducto(){
+
+    }
+    public void editarUnProducto(){
+
+    }
+}
