f5b0736e6ca21815a07508a3c22c321904d10658
abel-02 <abelaquino02j@gmail.com>
2025-01-13 11:44:46 -0300

    Creacion del controlador de cliente

---
+++ tp_Final/src/main/java/com/redo/tp_Final/controllers/ClienteController.java
@@ -0,0 +1,25 @@
+package com.redo.tp_Final.controllers;
+
+import com.redo.tp_Final.models.Cliente;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ClienteController {
+    public void registrarCliente(){
+
+    }
+    public List<Cliente> devolverListaCompleta(){
+        return null;
+    }
+    public Cliente devolverCliente(){
+        return null;
+    }
+    public void eliminarCliente(){
+
+    }
+    public void editarCliente(){
+
+    }
+}
