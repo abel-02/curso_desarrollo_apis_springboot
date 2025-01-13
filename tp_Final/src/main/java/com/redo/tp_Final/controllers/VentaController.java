ab43d7c9de24f43b99cd98fbd528244ab1da2706
abel-02 <abelaquino02j@gmail.com>
2025-01-13 11:46:28 -0300

    Creacion del controlador de venta

---
+++ tp_Final/src/main/java/com/redo/tp_Final/controllers/VentaController.java
@@ -0,0 +1,26 @@
+package com.redo.tp_Final.controllers;
+
+import com.redo.tp_Final.models.Cliente;
+import com.redo.tp_Final.models.Venta;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class VentaController {
+    public void registrarVenta(){
+
+    }
+    public List<Venta> devolverListaCompleta(){
+        return null;
+    }
+    public Venta devolverVenta(){
+        return null;
+    }
+    public void eliminarVenta(){
+
+    }
+    public void editarVenta(){
+
+    }
+}
