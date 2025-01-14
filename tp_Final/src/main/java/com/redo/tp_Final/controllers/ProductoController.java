ff30b4340336b460462265ffa3de360e8914bb95
abel-02 <abelaquino02j@gmail.com>
2025-01-14 19:50:29 -0300

    Endpoints de producto

--- tp_Final/src/main/java/com/redo/tp_Final/controllers/ProductoController.java
+++ tp_Final/src/main/java/com/redo/tp_Final/controllers/ProductoController.java
@@ -3,29 +3,36 @@ package com.redo.tp_Final.controllers;
 import com.redo.tp_Final.models.Producto;
 import com.redo.tp_Final.services.ServiceProducto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/productos")
 public class ProductoController {
     @Autowired
     private ServiceProducto servicio;
-    public void crearProducto(){
+    @PostMapping("/crear")
+    public void crearProducto(@RequestBody Producto producto){
 
     }
+    @RequestMapping("")
     public List<Producto> devolverListaCompleta(){
         return null;
     }
-    public Producto devolverUnProducto(){
+    @RequestMapping("/{idProducto}")
+    public Producto devolverUnProducto(@PathVariable Long idProducto){
        return null;
     }
-    public void eliminarUnProducto(){
+    @DeleteMapping("/{idProducto}")
+    public void eliminarUnProducto(@PathVariable Long idProducto){
 
     }
-    public void editarUnProducto(){
+    @PutMapping("/{idProducto}")
+    public void editarUnProducto(@PathVariable Long idProducto){
 
     }
+    @RequestMapping("/falta_stock")
     public List<Producto> productosConCantidadMenorA(int cantidad){
         return servicio.devolverProductosConCantidadMenorA(cantidad);
     }
