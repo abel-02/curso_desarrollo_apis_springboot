55f226f4b1b3f8952c0e2b0afe1f80c9d4556b80
abel-02 <abelaquino02j@gmail.com>
2025-01-14 20:21:11 -0300

    Repositorio de ventas

---
+++ tp_Final/src/main/java/com/redo/tp_Final/repository/VentaRepository.java
@@ -0,0 +1,9 @@
+package com.redo.tp_Final.repository;
+
+import com.redo.tp_Final.models.Venta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VentaRepository extends JpaRepository<Venta, Long> {
+}
